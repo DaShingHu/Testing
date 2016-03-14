32b1c7b308f34f69bd1f917bfb3f2c9dfe2cdd91
Dustin Hu <Tenteki@users.noreply.github.com>
2016-03-14 14:46:21 -0400

    Initial commit

---
+++ Testing/Testing/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Testing
+{
+    public partial class Form1 : Form
+    {
+        player newChar = new player(Testing.Properties.Resources.mc);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+ 
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                newChar.move("W");
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                newChar.move("E");
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                newChar.move("N");
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                newChar.move("S");
+            }
+            newChar.draw(ref panel1);
+        }
+    }
+}
---
+++ Testing/Testing/player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Testing
+{
+    class player
+    {
+        Bitmap self;
+        int x;
+        int y;
+
+        public player()
+        {
+            // Default constructor
+            self = new Bitmap(64, 64);
+        }
+        public player(Image inputImage)
+        {
+            self = new Bitmap(inputImage);
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public void move (String direction)
+        {
+            // E = +1
+            // W = -1
+            // N = +1
+            // S = -1
+            if (direction == "E")
+            {
+                this.x++;
+            }
+            else if (direction == "W")
+            {
+                this.x--;
+            }
+            else if (direction == "N")
+            {
+                this.y++;
+            }
+            else
+            {
+                this.y--;
+            }
+        }
+        public void draw(ref Panel target) {
+
+            Graphics g = target.CreateGraphics();
+            g.DrawImage(this.self, this.x, this.y);
+        }
+    }
+}
